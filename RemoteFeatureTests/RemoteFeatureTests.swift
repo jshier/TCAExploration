8e9a236c2dfa3c8313e464b6f86e404af9a0d1fc
Jon Shier <jon@jonshier.com>
2023-10-18 17:06:00 -0400

    Test cleanup.

--- RemoteFeatureTests/RemoteFeatureTests.swift
+++ RemoteFeatureTests/RemoteFeatureTests.swift
@@ -27,17 +27,20 @@ final class RemoteFeatureTests: XCTestCase {
     }
     store.useMainSerialExecutor = true
 
+    // When: the user taps the toggle button.
     await store.send(.toggleListeningButtonTapped) {
       $0.isListening = true
+      // As a computed property we must assert manually.
+      XCTAssertEqual($0.toggleListeningButtonTitle, "Stop Listening")
     }
 
+    // Then: Initial values are received.
     await store.receive(.receiveCommandStatus(.none)) {
       $0.commandSummary = "Command idle"
     }
     await store.receive(.receiveHVACSettings(.init(temperature: 72, isDefrostOn: false))) { state in
       state.currentTemperature = "72°F"
     }
-
     await store.receive(.receiveVehicleStatus(.init(doors: .open, windows: .closed, odometer: 1234))) {
       $0.currentMileage = "1234 mi"
     }
@@ -45,30 +48,35 @@ final class RemoteFeatureTests: XCTestCase {
       $0.chargingSummary = "Not charging!"
     }
 
+    // Then: a second elapses
     await clock.advance(by: .seconds(1))
 
     await store.receive(.receiveCommandStatus(.inFlight)) {
       $0.commandSummary = "Command in flight"
     }
     await store.receive(.receiveHVACSettings(.init(temperature: 72, isDefrostOn: false)))
-
     await store.receive(.receiveVehicleStatus(.init(doors: .open, windows: .closed, odometer: 1234)))
     await store.receive(.receiveElectricStatus(.init(plugin: .unplugged)))
 
+    // Then: a second elapses.
     await clock.advance(by: .seconds(1))
 
+    // Then: additional values are received.
     await store.receive(.receiveCommandStatus(.none)) {
       $0.commandSummary = "Command idle"
     }
     await store.receive(.receiveHVACSettings(.init(temperature: 72, isDefrostOn: false)))
-
     await store.receive(.receiveVehicleStatus(.init(doors: .open, windows: .closed, odometer: 1234)))
     await store.receive(.receiveElectricStatus(.init(plugin: .unplugged)))
 
+    // Then: the user taps the toggle button again.
     await store.send(.toggleListeningButtonTapped) {
       $0.isListening = false
+      // As a computed property we must assert manually.
+      XCTAssertEqual($0.toggleListeningButtonTitle, "Start Listening")
     }
 
+    // Then: store has finished processing all effects.
     await store.finish()
   }
 }
